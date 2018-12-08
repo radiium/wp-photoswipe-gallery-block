9fce62030a30d97b48150deb7eb0d65636011705
radiium <remip.dev@gmail.com>
2018-12-08 19:37:57 +0100

    Test

--- plugin.php
+++ plugin.php
@@ -1,17 +1,17 @@
 <?php
  /**
   *
- * Plugin Name: PhotoSwipe Gallery block
- * Plugin URI: https://github.com/radiium/wp-photoswipe-gallery-block
- * Description: Gutenberg gallery block with PhotoSwipe
- * Version: 1.0.0
- * Author: radiium
- * Author URI: https://github.com/radiium
- * License: GPL2+
- * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
- *
- * @package radiium
- */
+  * Plugin Name: PhotoSwipe Gallery block
+  * Plugin URI: https://github.com/radiium/wp-photoswipe-gallery-block
+  * Description: Gutenberg gallery block with PhotoSwipe
+  * Version: 1.0.0
+  * Author: radiium
+  * Author URI: https://github.com/radiium
+  * License: GPL2+
+  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+  *
+  * @package radiium
+  */
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
